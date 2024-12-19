de6e8b7024f171e37a270a37ead1c7b509b9d997
anbonnn <anboshdeee@gmail.com>
2024-12-19 16:25:08 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const targetId = e.target.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
